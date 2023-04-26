43efb6337080462a471e7b01f1d2b3221753bc69
Barron V Brock <bmansk8@yahoo.com>
2023-04-26 10:21:46 -0400

    animate hook refactor

--- src/hooks/animateHook.ts
+++ src/hooks/animateHook.ts
@@ -1,25 +1,38 @@
 import { useEffect, useRef } from "react";
 
-export default function useAnimateHook(elementsToObserve: React.RefObject<HTMLDivElement>[]){
-    const observer = useRef<IntersectionObserver>();
-  
-    useEffect(() => {
-      observer.current = new IntersectionObserver((entries)=>{
-          entries.forEach((entry)=>{
-            if(entry.isIntersecting){
-              entry.target.classList.add("show")
-            }else{
-              entry.target.classList.remove("show")
-            }
-          })
-        })
-  
-      elementsToObserve.forEach((ref)=> {
-        if(ref.current && observer.current) observer.current.observe(ref.current)
-      })
-    
-      return () => {
-        if(observer.current) observer.current.disconnect()
-      }
-    }, [elementsToObserve])
+export default function useAnimateHook(
+  elementsToObserve: React.RefObject<Element>[]
+) {
+  const observer = useRef<IntersectionObserver>();
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(observerCallback);
+
+    startObservingFunc(elementsToObserve, observer.current);
+
+    return () => {
+      if (observer.current) observer.current.disconnect();
+    };
+  }, [elementsToObserve]);
 }
+
+function observerCallback(entries: IntersectionObserverEntry[]) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting ) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+}
+
+function startObservingFunc(
+  elementsToObserve: React.RefObject<Element>[],
+  observer: IntersectionObserver
+) {
+  elementsToObserve.forEach((ref) => {
+    if (ref.current && observer) observer.observe(ref.current);
+  });
+}
+
+//&& entry.intersectionRatio >= .8
